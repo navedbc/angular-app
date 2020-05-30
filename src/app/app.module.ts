b5a03209c38bf75a5a64f248b905e2788ab55530
navedbc <navedbc@gmail.com>
2020-05-29 17:28:05 -0700

    added service model and basic structure for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './components/shopping-cart/products/products.component';
+import { ProductItemComponent } from './components/shopping-cart/products/product-item/product-item.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HeaderComponent,
+    ShoppingCartComponent,
+    ProductsComponent,
+    ProductItemComponent,
+    CartComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule
